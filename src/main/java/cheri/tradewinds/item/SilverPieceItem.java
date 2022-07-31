95c90d95ef7d8eee93f3d0e158c80a66a3393357
Cheri <kkiyo2@hotmail.com>
2022-07-31 08:45:50 +0800

    added silver piece

---
+++ src/main/java/cheri/tradewinds/item/SilverPieceItem.java
@@ -0,0 +1,23 @@
+package cheri.tradewinds.item;
+
+import java.util.List;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+public class SilverPieceItem extends Item {
+	public SilverPieceItem(Settings settings) {
+		super(settings);
+	}
+
+	@Override
+	public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+		tooltip.add( new TranslatableText("item.tradewinds.silver_piece.tooltip1").formatted(Formatting.DARK_GRAY) );
+		tooltip.add( new TranslatableText("item.tradewinds.silver_piece.tooltip2").formatted(Formatting.DARK_GRAY) );
+	}
+}
--- src/main/java/cheri/tradewinds/register/ModItems.java
+++ src/main/java/cheri/tradewinds/register/ModItems.java
@@ -1,6 +1,7 @@
 package cheri.tradewinds.register;
 
 import cheri.tradewinds.item.GoldPieceItem;
+import cheri.tradewinds.item.SilverPieceItem;
 import cheri.tradewinds.item.TradersManualItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.util.Identifier;
@@ -9,10 +10,12 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
 	public static final GoldPieceItem GOLD_PIECE = new GoldPieceItem(new FabricItemSettings().group(ModItemGroup.GENERAL));
+	public static final SilverPieceItem SILVER_PIECE = new SilverPieceItem(new FabricItemSettings().group(ModItemGroup.GENERAL));
 	public static final TradersManualItem TRADERS_MANUAL = new TradersManualItem(new FabricItemSettings().group(ModItemGroup.GENERAL));;
 
 	public static void register() {
 		Registry.register(Registry.ITEM, new Identifier("tradewinds", "gold_piece"), GOLD_PIECE);
+		Registry.register(Registry.ITEM, new Identifier("tradewinds", "silver_piece"), SILVER_PIECE);
 		Registry.register(Registry.ITEM, new Identifier("tradewinds", "traders_manual"), TRADERS_MANUAL);
 	}
 }
